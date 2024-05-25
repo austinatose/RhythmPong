bf953cfbe7f8162d5e9f99f13addfdb0fda91b43
austinato <austinliuzirui@gmail.com>
2024-05-25 16:13:50 +0800

    opponent and new mechanics
    remix 3 bpm 145
    
    potential features:
    - juiciness, with user adjustable settings
    - score and combos
    - bpm detection
    - title and song selection
    - a lot of settings

--- game.js
+++ game.js
@@ -1,13 +1,18 @@
 class Game {
   constructor(interval) {
-    this.interval = 1
+    this.interval = 60/145 // 145 bpm
     this.ball = new Ball(width / 2 - 120, height / 2 - 310)
     this.table = new Table()
     this.paddle = new Paddle()
+    this.opponent = new Opponent()
     this.targetloc = createVector()
-    this.targets = [-200, 0, 200]
     this.init = true;
     this.startframe = frameCount;
+    this.hit = false;
+    this.missedlasttime = false;
+    this.ballmovingtowardsplayer = true;
+    this.score = 0;
+    this.distance = 10000
   }
 
   render() {
@@ -15,27 +20,56 @@ class Game {
     this.table.render()
     this.ball.render()
     this.paddle.render()
+    this.opponent.render()
+    text(this.distance, 100, 100)
+    text(this.score, 100, 200)
+    if (this.missedlasttime) {
+      text("Missed", 100, 150)
+    } else if (this.distance < 25) {
+      text("Perfect", 100, 150)
+    } else if (this.distance < 60) {
+      text("Good", 100, 150)
+    } else if (this.distance < 100) {
+      text("Ok", 100, 150)
+    } else {
+      text("Bad", 100, 150)
+    }
     if (this.init) {
-      this.targetloc.y = height / 2 + 350
-      this.targetloc.x = width / 2 + this.targets[Math.floor(Math.random() * this.targets.length)]
+      this.targetloc.y = height / 2 + 340
+      this.targetloc.x = width / 2 + random(-150, 150)
       this.ball.determineVelocity(this.targetloc, this.interval)
       this.init = false
     }
     // return
-    console.log(this.ball.pos)
-    if (this.ball.pos.x < this.paddle.x + 20 && this.ball.pos.x > this.paddle.x - 20 && this.ball.pos.y < this.paddle.y + 20 && this.ball.pos.y > this.paddle.y - 20) { // collided
-      console.log("hit")
+    if (this.ball.pos.x < this.paddle.x + 30 && this.ball.pos.x > this.paddle.x - 30 && this.ball.pos.y < this.paddle.y + 30 && this.ball.pos.y > this.paddle.y - 30 && !this.hit) { // collided
+      this.distance = dist(this.ball.pos.x, this.ball.pos.y, this.targetloc.x, this.targetloc.y)
+      console.log("hit", this.distance)
       this.targetloc.y = height / 2 - 310
-      this.targetloc.x = width / 2 - 120
+      this.targetloc.x = width / 2 + random(-150, 150) // -150 to 150
       this.ball.determineVelocity(this.targetloc, (this.interval * 60 * 2 - frameCount + this.startframe) / 60)
+      this.hit = true
+      this.missedlasttime = false
+      this.ballmovingtowardsplayer = true
     }
     this.ball.move()
-    
-    // placeholder
-    if (frameCount - this.startframe == Math.round(this.interval * 60 * 2)) {
-      this.ball = new Ball(width / 2 - 120, height / 2 - 310, this.interval)
+    if (this.ballmovingtowardsplayer) {
+      this.opponent.targetloc = this.targetloc
+      this.opponent.move()
+    }
+    // if exceeded beat (missed ball)
+    if (frameCount - this.startframe == Math.round(this.interval * 60 * 2)) {      
+      if (this.hit) {
+        this.ball = new Ball(this.targetloc.x, height / 2 - 310, this.interval)
+        this.hit = false
+      } else {
+        this.ball = new Ball(width / 2 - 120, height / 2 - 310, this.interval)
+        console.log("missed")
+        this.opponent.x = width / 2 - 120
+        this.missedlasttime = true
+      }
       this.startframe = frameCount
       this.init = true
+      this.ballmovingtowardsplayer = false
     }
   }
 }
--- opponent.js
+++ opponent.js
@@ -1,21 +1,24 @@
-class Paddle {
+class Opponent {
   constructor() {
     this.x = 0
-    this.y = 0
+    this.y = height/2 - 350
+    this.targetloc = createVector()
+  }
+
+  move() {
+    this.x += (this.targetloc.x - this.x) * 0.1
   }
 
   render() {
-    this.x = mouseX
-    this.y = mouseY
     push()
     translate(this.x, this.y)
-    let rotationAngle = -(width/2 - mouseX - 80) / 400
+    let rotationAngle = -(width/2 - this.x - 80) / 400
     constrain(rotationAngle, -0.5625, 0.5625)
-    rotate(rotationAngle)
+    rotate(rotationAngle + PI)
     strokeWeight(8)
     fill('light brown')
     quad(-10, 35, 10, 35, 10, 65, -10, 65)
-    fill('red')
+    fill('blue')
     ellipse(0, 0, 70, 70)
     line(30, 15, -30, 15)
     pop()
